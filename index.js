88f8e7dded2248f731af129125c715f9e341dd73
Horus S. Rico <horus.sandoval@carpathialab.com>
2019-03-18 19:00:26 -0600

    Contador terminado, pero generando conflictos

--- index.js
+++ index.js
@@ -4,8 +4,12 @@
 
 /*
 function goBack(){
+	//No estoy de acuerdo
   document.getElementById("showMovies").style.display = 'none';
   document.getElementById("howYouFeel").style.display = 'block';
+  //puede que con ésto sí
+  
   location.reload("howYouFeel");
 }
+//pero definitivamente ésto no
 document.getElementById("btnBack").addEventListener('click',goBack); */
