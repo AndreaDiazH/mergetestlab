fe5f8baf7a5fffacc9c8d7b53d4a0e33864d9e12
Andrea Díaz Hernández <andreanibaldi@felina.neko.org>
2019-03-18 18:58:39 -0600

    prueba con conflictos

--- index.js
+++ index.js
@@ -2,6 +2,16 @@
 
 //esta es mi funcion de volver con el reload de la pagina que resolvi solita magicamente
 
+function showFirstScreen(Mood){
+ 
+  document.getElementById(Mood).style.display = "block";
+   document.getElementById("howYouFeel").style.display = "none";
+   document.getElementById("showMovies").style.display = "block";
+}
+document.getElementById("btnSmile").addEventListener('click',()=>{showFirstScreen("movieCardsSmile")});
+document.getElementById("btnSquint").addEventListener('click',()=>{showFirstScreen("movieCardsBored")});
+document.getElementById("btnSad").addEventListener('click',()=>{showFirstScreen("movieCardsSad")});
+
 /*
 function goBack(){
   document.getElementById("showMovies").style.display = 'none';
